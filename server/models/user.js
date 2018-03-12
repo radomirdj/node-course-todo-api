f3a4e2c71242ef519b6df45b66894d87e0c52600
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2018-03-12 17:01:36 +0100

    Added /todo post handler

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+let {mongoose} = require('../db/mongoose.js');
+
+User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {User};
---
+++ server/server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {Todo} = require('./models/todo');
+let {Uset} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    //console.log(`Can't save todo: ${err}`)
+    res.status(400).send(err);
+  });
+});
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on log ${3000}`);
+});
