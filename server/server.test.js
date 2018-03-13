3fed3fc7beae18dcc0324005461243c56a163998
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2018-03-13 12:16:15 +0100

    Has problem in server.test.js

--- server/server.test.js
+++ server/server.test.js
@@ -15,20 +15,20 @@ describe("Post /todo", () => {
     request(app)
       .post("/todos")
       .send({text})
-      //.expect(200)
-      // .expect(res => {
-      //   expect(res.body.text).toBe(text);
-      // })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.text).toBe(text);
+      })
       .end((err, res) => {
         if(err) {
           done(err);
         }
-        //Todo.find().then((todos)=>{
-          //expect(todos.length).toBe(1);
-          //expect(todos[0].text).toBe(text);
-        //  done();
-      //  }
-      //);
+        Todo.find().then((todos)=>{
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+         done();
+       }
+      );
       })
       .catch((err) =>{done(err)});
   });
